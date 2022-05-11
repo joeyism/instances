eb793bcb9df5578d4afb31014b0c5b5d53eabd55
Joey <sham.joey@gmail.com>
2022-05-11 14:27:45 +0200

    Update base_customization.sh

--- customization-scripts/base_customization.sh
+++ customization-scripts/base_customization.sh
@@ -16,7 +16,7 @@ cat <<EOT >> ~/.bashrc
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
-PS1='\[\033[01;32m\]\u@\h\[\033[00m\]\[\033[01;33m\]$(parse_git_branch):\[\033[01;34m\]\W\[\033[00m\]$ '
+PS1='\[\033[01;32m\]\u@\h\[\033[00m\]\[\033[01;33m\]:\[\033[01;34m\]\W\[\033[01;33m\]$(parse_git_branch)\[\033[00m\]$ '
 export PROMPT_COMMAND="history -a; history -n"
 export EDITOR=vim
 EOT
