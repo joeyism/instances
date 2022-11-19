315f68def67835eb0ef4a4c20290db22f6b2bdb7
Joey <sham.joey@gmail.com>
2022-11-20 00:48:44 +0100

    Update base_customization_apt.sh

--- customization-scripts/base_customization_apt.sh
+++ customization-scripts/base_customization_apt.sh
@@ -1,3 +1,3 @@
 sudo apt update
-sudo apt -y install htop tmux git
+sudo apt -y install htop tmux git kitty-terminfo
 curl https://raw.githubusercontent.com/joeyism/instances/master/customization-scripts/base_customization.sh | bash
