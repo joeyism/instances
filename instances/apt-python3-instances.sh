8aae5f778d78b6adec62707ea25bc6aecb01eab9
Joey <sham.joey@gmail.com>
2022-12-07 22:49:40 +0100

    Update apt-python3-instances.sh

--- instances/apt-python3-instances.sh
+++ instances/apt-python3-instances.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 pip3 install --user numpy ipython ipdb virtualenv
 curl https://raw.githubusercontent.com/joeyism/instances/master/customization-scripts/base_customization_apt.sh | bash
