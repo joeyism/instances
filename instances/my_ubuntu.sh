a469f378c0fd61a3a0c93bee535adb0366d14a92
Joey <sham.joey@gmail.com>
2023-04-25 16:45:36 +0200

    Update my_ubuntu.sh

--- instances/my_ubuntu.sh
+++ instances/my_ubuntu.sh
@@ -47,4 +47,3 @@ gsettings set org.gnome.shell.ubuntu color-scheme prefer-dark
 gsettings set org.gnome.desktop.interface gtk-theme Yaru-dark # Legacy apps, can specify an accent such as Yaru-olive-dark
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark # new apps
 gsettings reset org.gnome.shell.ubuntu color-scheme
-
