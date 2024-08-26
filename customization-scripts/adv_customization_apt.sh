0ae12b20614bdaff8780342aeb9ad63eaf7692b6
Joey <sham.joey@gmail.com>
2024-08-26 09:45:04 +0200

    Update adv_customization_apt.sh

--- customization-scripts/adv_customization_apt.sh
+++ customization-scripts/adv_customization_apt.sh
@@ -3,5 +3,7 @@ curl https://raw.githubusercontent.com/joeyism/instances/master/customization-sc
 
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install -y neovim python3-neovim
+wget https://github.com/neovim/neovim-releases/releases/download/v0.10.1/nvim.appimage
+chmod +x nvim.appimage
+sudo mv nvim.appimage /usr/local/bin/nvim
 nvim +PackerSync
