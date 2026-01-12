7757355bb611ef8c2764648958ff02089d528885
Joey <sham.joey@gmail.com>
2026-01-12 19:51:07 +0100

    Update base_customization_yum.sh

--- customization-scripts/base_customization_yum.sh
+++ customization-scripts/base_customization_yum.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo yum install -y tmux
-sudo yum install -y git 
+sudo yum install -y tmux git curl kitty-terminfo
 curl https://raw.githubusercontent.com/joeyism/instances/master/customization-scripts/base_customization.sh | bash
 echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ '" >> .bashrc
 . ~/.bashrc
