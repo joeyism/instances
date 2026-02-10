b9f1d81f037a0dc165fbcae6363b39ff27f43e34
Joey <7503144+joeyism@users.noreply.github.com>
2026-02-10 19:22:24 +0100

    Update base_customization_yum.sh for kitty terminfo
    Download and install kitty terminfo before installing packages.

--- customization-scripts/base_customization_yum.sh
+++ customization-scripts/base_customization_yum.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo yum install -y tmux git curl kitty-terminfo
+curl -o /tmp/kitty.terminfo https://raw.githubusercontent.com/kovidgoyal/kitty/master/terminfo/kitty.terminfo
+tic -x -o ~/.terminfo /tmp/kitty.terminfo
+sudo yum install -y tmux git curl
 curl https://raw.githubusercontent.com/joeyism/instances/master/customization-scripts/base_customization.sh | bash
 echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ '" >> .bashrc
 . ~/.bashrc
